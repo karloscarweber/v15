7841655004f4585da92825405a2ed6e38b4681c1
karloscarweber <me@kow.fm>
2020-08-14 02:10:42 -0600

    Improved layout for a bunch of pages. removed derelict javascript code. fixed a couple layout and typography issues.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,44 @@
 
-let app = function(){
-
-  // Monitor open or close the menu.
-  (function(){
-    let menubutton = document.querySelector(".expand_menu_button");
-    let extendedmenu = document.querySelector(".expanding_menu_container");
-    let menu_backround = document.querySelector(".expanding_menu_background");
-
-    menubutton.onclick = function(){
-      if (_.contains(menubutton.classList, "opened") && !(_.contains(menubutton.classList, "transitioning"))) {
-        menubutton.classList.remove("opened");
-        extendedmenu.classList.remove("opened");
-        menu_backround.classList.remove("opened");
-
-        // keep the menu from being all crazy
-        menubutton.classList.add("transitioning");
-        extendedmenu.classList.add("transitioning");
-
-        setTimeout(function(){
-          menubutton.classList.remove("transitioning");
-          extendedmenu.classList.remove("transitioning");
-        }, 1550);
-      } else if ( !(_.contains(menubutton.classList, "transitioning"))){
-        menubutton.classList.add("opened");
-        extendedmenu.classList.add("opened");
-        menu_backround.classList.add("opened");
-
-        // keep the menu from being all crazy
-        menubutton.classList.add("transitioning");
-        extendedmenu.classList.add("transitioning");
-
-        setTimeout(function(){
-          menubutton.classList.remove("transitioning");
-          extendedmenu.classList.remove("transitioning");
-        }, 1550);
-      }
-    }
-
-  })();
-
-}
-
-window.onload = app;
+// let app = function(){
+//
+//   // Monitor open or close the menu.
+//   (function(){
+//     let menubutton = document.querySelector(".expand_menu_button");
+//     let extendedmenu = document.querySelector(".expanding_menu_container");
+//     let menu_backround = document.querySelector(".expanding_menu_background");
+//
+//     menubutton.onclick = function(){
+//       if (_.contains(menubutton.classList, "opened") && !(_.contains(menubutton.classList, "transitioning"))) {
+//         menubutton.classList.remove("opened");
+//         extendedmenu.classList.remove("opened");
+//         menu_backround.classList.remove("opened");
+//
+//         // keep the menu from being all crazy
+//         menubutton.classList.add("transitioning");
+//         extendedmenu.classList.add("transitioning");
+//
+//         setTimeout(function(){
+//           menubutton.classList.remove("transitioning");
+//           extendedmenu.classList.remove("transitioning");
+//         }, 1550);
+//       } else if ( !(_.contains(menubutton.classList, "transitioning"))){
+//         menubutton.classList.add("opened");
+//         extendedmenu.classList.add("opened");
+//         menu_backround.classList.add("opened");
+//
+//         // keep the menu from being all crazy
+//         menubutton.classList.add("transitioning");
+//         extendedmenu.classList.add("transitioning");
+//
+//         setTimeout(function(){
+//           menubutton.classList.remove("transitioning");
+//           extendedmenu.classList.remove("transitioning");
+//         }, 1550);
+//       }
+//     }
+//
+//   })();
+//
+// }
+//
+// window.onload = app;
