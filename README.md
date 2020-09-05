708920ad649a380c7dbd17641be97973d9852fe3
karloscarweber <me@kow.fm>
2020-09-05 05:04:45 -0600

    Made a brand new fancy menu that's responsive and has 3d effects.

--- README.md
+++ README.md
@@ -4,12 +4,6 @@ This is the fifteenth version of kowfm. Based, very very loosely, off of Casper.
 
 ![screenshot-desktop](/assets/screenshot-desktop.png)
 
-# To Do
-
-- [x] Make Book template
-- [ ] Make sidebar responsive
-- [ ] Make apps page
-
 ## Building the CSS
 
 So we're using Gulp start it by using Gulp. type `gulp default` into your console in the root after it's installed and you should be good for development.
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,45 @@
+let app = function(){
 
-// let app = function(){
-//
-//   // Monitor open or close the menu.
-//   (function(){
-//     let menubutton = document.querySelector(".expand_menu_button");
-//     let extendedmenu = document.querySelector(".expanding_menu_container");
-//     let menu_backround = document.querySelector(".expanding_menu_background");
-//
-//     menubutton.onclick = function(){
-//       if (_.contains(menubutton.classList, "opened") && !(_.contains(menubutton.classList, "transitioning"))) {
-//         menubutton.classList.remove("opened");
-//         extendedmenu.classList.remove("opened");
-//         menu_backround.classList.remove("opened");
-//
-//         // keep the menu from being all crazy
-//         menubutton.classList.add("transitioning");
-//         extendedmenu.classList.add("transitioning");
-//
-//         setTimeout(function(){
-//           menubutton.classList.remove("transitioning");
-//           extendedmenu.classList.remove("transitioning");
-//         }, 1550);
-//       } else if ( !(_.contains(menubutton.classList, "transitioning"))){
-//         menubutton.classList.add("opened");
-//         extendedmenu.classList.add("opened");
-//         menu_backround.classList.add("opened");
-//
-//         // keep the menu from being all crazy
-//         menubutton.classList.add("transitioning");
-//         extendedmenu.classList.add("transitioning");
-//
-//         setTimeout(function(){
-//           menubutton.classList.remove("transitioning");
-//           extendedmenu.classList.remove("transitioning");
-//         }, 1550);
-//       }
-//     }
-//
-//   })();
-//
-// }
-//
-// window.onload = app;
+
+  console.log("run app")
+  // Monitor open or close the menu.
+  (function(){
+    let menubutton = document.querySelector(".scene");
+    // let extendedmenu = document.querySelector(".expanding_menu_container");
+    // let menu_backround = document.querySelector(".expanding_menu_background");
+
+    menubutton.onclick = function(){
+      if (_.contains(menubutton.classList, "opened")) {
+        menubutton.classList.remove("opened");
+        // extendedmenu.classList.remove("opened");
+        // menu_backround.classList.remove("opened");
+        //
+        // // keep the menu from being all crazy
+        // menubutton.classList.add("transitioning");
+        // extendedmenu.classList.add("transitioning");
+        //
+        // setTimeout(function(){
+        //   menubutton.classList.remove("transitioning");
+        //   extendedmenu.classList.remove("transitioning");
+        // }, 1550);
+      } else {
+        menubutton.classList.add("opened");
+        // extendedmenu.classList.add("opened");
+        // menu_backround.classList.add("opened");
+        //
+        // // keep the menu from being all crazy
+        // menubutton.classList.add("transitioning");
+        // extendedmenu.classList.add("transitioning");
+        //
+        // setTimeout(function(){
+        //   menubutton.classList.remove("transitioning");
+        //   extendedmenu.classList.remove("transitioning");
+        // }, 1550);
+      }
+    }
+
+  })();
+
+}
+
+window.onload = app;
