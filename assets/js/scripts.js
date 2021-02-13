7ba50ca3faf823716fbc3f17754ec820ebceef0d
karloscarweber <me@kow.fm>
2021-02-13 14:33:26 -0700

    Improvements to the book Index Page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 // stimulus initializtion
 import { Application } from "stimulus"
 
-import HelloController from "./controllers/hello_controller"
+// import HelloController from "./controllers/hello_controller"
 // import ClipboardController from "./controllers/clipboard_controller"
 
 const application = Application.start()
-application.register("hello", HelloController)
+// application.register("hello", HelloController)
 // application.register("clipboard", ClipboardController)
 
 
